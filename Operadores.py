f2f35d45abb313272289c1d2a71f722996deed5d
Rerisson H R Silva <148692810+Rerisson666@users.noreply.github.com>
2024-01-19 13:37:03 -0300

    Cursando em Vídeo - Python

---
+++ Operadores.py
@@ -0,0 +1,23 @@
+# .Format pra apresentação de resultados de forma simples
+
+n = int(input('Digite um número: '))
+a = n - 1
+b = n + 1
+print('Analisando o valor {}, seu anecesor é {} e o sucessor é {}'.format(n, a, b))
+
+# Calculos Aritiméticos básicos
+
+n = int(input('Digite um número: '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print('O doblo de {} vale {}'.format(n, ))
+print('O triplo de {} vale {}. \nA raiz dquadrada de {} é igual a {}.'.format(n*3, d, n, r))
+
+# Simplificando  Calculas Aritiméticos básicos
+
+n = int(input('Digite um número: '))
+print('O doblo de {} vale {}'.format(n, (n*2)))
+print('O triplo de {} vale {}. \nA raiz dquadrada de {} é igual a {}.'.format(n, (n*3), n, pow(n,(1/2))))
+
+# Lista de Resultados - Modelo simples de demonstrativo de Resultado
