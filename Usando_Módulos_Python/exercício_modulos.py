f079b7af16f7a9eba2062cf62380ec5d5ca82919
Rerisson H R Silva <148692810+Rerisson666@users.noreply.github.com>
2024-01-31 14:03:58 -0300

    atualizando conhecimento

--- Usando_Módulos_Python/exercício_modulos.py
+++ Usando_Módulos_Python/exercício_modulos.py
@@ -35,3 +35,39 @@ cosseno = cos(math.radians(ângulo))
 print('O ângulo de {} tem o COSSENO de {:.2f}'.format(ângulo, cosseno))
 tangente = tan(math.radians(ângulo))
 print('O ângulo de {} tem o TANGENTE de {:.2f}'.format(ângulo, tangente))
+
+## Exercício 04 ##
+#Crie um programa que some o comprimento e mostre a hipotenusa de um triângulo
+
+## Exercício 05 ##
+#Crie um sorteador de valores aleatórios
+from random import choice
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundoo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
+lista = [n1, n2, n3, n4]
+escolhido = choice(lista)
+print('O aluno escolhido foi {}'.format(escolhido))
+
+## Exercício 06 ##
+#Crie um sorteador de ordem aleatória dos valores
+from random import shuffle
+n1 = str(input('Primeiro nome: '))
+n2 = str(input('Segundo nome: '))
+n3 = str(input('Terceiro nome: '))
+n4 = str(input('Quanto nome: '))
+lista = [n1, n2, n3, n4]
+shuffle(lista)
+print('A ordem de apresentação será ')
+print(lista)
+
+## Exercício 07 ##
+#Crie um tocador MP3 dentro do Python
+import pygame
+pygame.init()
+pygame.mixer.music.load('test.mp3')
+pygame.mixer.music.play
+pygame.event.wait()
+
+
